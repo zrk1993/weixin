f131530f9e106490b7f9eeec8142e6bb61ef63fa
仁焜 <1051455824@qq.com>
2016-07-03 12:56:23 +0800

    7.3

--- routes/weixin.js
+++ routes/weixin.js
@@ -1,9 +1,17 @@
 var express = require('express');
+var URL = require('url');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('true');
+    var arg = URL.parse(req.url, true).query;
+    res.send(checkSignature(arg));
 });
-
+//检验signature对请求进行校验（下面有校验方式）。若确认此次GET请求来自微信服务器，请原样返回echostr参数内容
+function checkSignature(arg) {
+    $signature = arg["signature"];
+    $timestamp = arg["timestamp"];
+    $nonce = arg["nonce"];
+    return $signature;
+}
 module.exports = router;

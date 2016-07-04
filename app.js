2d767c4d885ca34070544f02ee9256fa528bb233
仁焜 <1051455824@qq.com>
2016-07-04 18:47:36 +0800

    p

--- app.js
+++ app.js
@@ -23,8 +23,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', routes);
 app.use('/weixin', weixin);

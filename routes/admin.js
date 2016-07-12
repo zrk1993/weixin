9d59a99b70e86a193f2452c34679c1917eb29065
renkun <1051455824@qq.com>
2016-07-12 14:58:00 +0800

    a

--- routes/admin.js
+++ routes/admin.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
-router.use(express.static(path.join(__dirname, 'admin')));
+//router.use(express.static(path.join(__dirname)));
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {

f5620c7e2e23d3202581560f0b4c6e9512c57829
仁焜 <1051455824@qq.com>
2016-08-12 20:42:23 +0800

    qq

--- bin/data/myMongoose.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by RK on 2016/7/12.
- */
-var config=require('../config');
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/mydb');
-
-module.exports = mongoose;
